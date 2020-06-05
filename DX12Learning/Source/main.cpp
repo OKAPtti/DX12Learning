e758554c2e97767654824fdcd71c14613c85d4e0
Kazuma Okai <kd1251505@st.kobedenshi.ac.jp>
2020-06-05 20:41:15 +0900

    【ソースコード追加】 main.cpp
    【内容】
    デバッグ時のみ動作するコンソールを表示

---
+++ DX12Learning/Source/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <Windows.h>
+#include <wrl.h>
+
+#ifdef _DEBUG
+#include <iostream>
+#endif // _DEBUG
+
+//@brief コンソール画面にフォーマット付き文字列を表示.
+//@pram format フォーマット(%dとか%fとかの).
+//@pram 可変長引数.
+//@remarks この関数はデバッグ用です。デバッグ時にしか動作しません.
+void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	vprintf(format, valist);
+	va_end(valist);
+#endif // _DEBUG
+
+}
+
+#ifdef _DEBUG
+int main()
+{
+	
+
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+#endif
+	DebugOutputFormatString("Show window test.");
+	getchar();
+	return 0;
+}
+
+
